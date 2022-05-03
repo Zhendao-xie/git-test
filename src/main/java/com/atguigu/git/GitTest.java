a8b62da6a26a97977c7b84c02a7894475cc81c94
Frank <Frank@163.com>
2022-05-03 20:38:15 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @author kunyu xie
+ * @creat 2022/5/3 20:35
+ */
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
